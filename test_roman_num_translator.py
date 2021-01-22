efc51df277ff7f5e076df908aaec278929f07c9c
shura <strashila@gmail.com>
2021-01-22 23:54:39 +0200

    Added unit tests for roman translator

--- test_roman_num_translator.py
+++ test_roman_num_translator.py
@@ -1,6 +1,11 @@
-
+# testing roman num translator
 
 import roman_num_translator
 
+class TestRomanNumTranslator:
+    def test_16(self):
+        assert 16 == roman_num_translator.roman_to_arabic("XVI")
 
+    def test_2019(self):
+        assert 2019 == roman_num_translator.roman_to_arabic("MMXIX")
 
