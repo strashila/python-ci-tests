a3d61e59507d79132d2062d19f6ec696c162aeef
shura <strashila@gmail.com>
2021-01-24 22:24:21 +0200

    added placeholder for arab to roman

--- test_roman_arabic_num_translator.py
+++ test_roman_arabic_num_translator.py
@@ -1,11 +1,17 @@
 # testing roman num translator
 
 import roman_num_translator
+import arabic_num_translator
 
 
-class TestRomanNumTranslator:
+class TestRomanToArabicTranslator:
     def test_16(self):
         assert 16 == roman_num_translator.roman_to_arabic("XVI")
 
     def test_2019(self):
         assert 2019 == roman_num_translator.roman_to_arabic("MMXIX")
+
+
+class TestArabicToRomanTranslator:
+    def test_8(self):
+        assert "VIII" == arabic_num_translator.arabic_to_roman(8)
