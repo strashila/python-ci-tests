ece697767c261f50fc29b72367f8433749743cb1
shura <strashila@gmail.com>
2021-01-29 22:41:51 +0200

    added roman to arabic

--- test_arabic_num_translator.py
+++ test_arabic_num_translator.py
@@ -6,3 +6,6 @@ import arabic_num_translator
 class TestArabicToRomanTranslator:
     def test_8(self):
         assert "VIII" == arabic_num_translator.arabic_to_roman(8)
+
+    def test_2021(self):
+        assert "MMXXI" == arabic_num_translator.arabic_to_roman(2021)
